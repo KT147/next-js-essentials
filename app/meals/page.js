35d83c612135ee8da97e7cdaa19ad691598d5528
KT147 <kevin.tammm@gmail.com>
2026-01-09 13:13:17 +0200

    Adding loader.js, <Suspense/> to load things that are not related to HTTP requests

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,12 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/MealsGrid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-async function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
 
-  const meals = await getMeals()
+  return <MealsGrid meals={meals} />;
+}
 
-
+function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +24,9 @@ async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
